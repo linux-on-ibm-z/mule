6a00683cb8fd87cce1c39a99b62bb071785f3eb9
mbuchwald <m.e.buchwald@gmail.com>
2016-08-29 15:36:05 -0300

    Added licence to new files

--- core/src/main/java/org/mule/processor/chain/InterceptingChainLifecycleWrapperPathSkip.java
+++ core/src/main/java/org/mule/processor/chain/InterceptingChainLifecycleWrapperPathSkip.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.processor.chain;
 
 import org.mule.api.processor.MessageProcessor;
@@ -5,9 +11,6 @@ import org.mule.api.processor.MessageProcessorChain;
 
 import java.util.List;
 
-/**
- * Created by martinbuchwald on 8/29/16.
- */
 public class InterceptingChainLifecycleWrapperPathSkip extends InterceptingChainLifecycleWrapper{
 
     public InterceptingChainLifecycleWrapperPathSkip(MessageProcessorChain chain,
--- core/src/main/java/org/mule/processor/chain/PathSkipMessageProcessorChainBuilder.java
+++ core/src/main/java/org/mule/processor/chain/PathSkipMessageProcessorChainBuilder.java
@@ -1,10 +1,13 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.processor.chain;
 
 import org.mule.api.processor.MessageProcessorChain;
 
-/**
- * Created by martinbuchwald on 8/29/16.
- */
 public class PathSkipMessageProcessorChainBuilder extends DefaultMessageProcessorChainBuilder {
     @Override
     protected MessageProcessorChain buildMessageProcessorChain(DefaultMessageProcessorChain chain)
